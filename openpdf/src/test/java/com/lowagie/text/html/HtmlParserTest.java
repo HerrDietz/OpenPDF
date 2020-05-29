42f6eb4dbc1ea0180f222d593dc61f8f67e439b1
HerrDietz <32846564+HerrDietz@users.noreply.github.com>
2020-05-29 08:00:34 +0200

    Added failing test to HtmlParser
    Created a failing test in order do provide a minimal failing example for an issue

--- openpdf/src/test/java/com/lowagie/text/html/HtmlParserTest.java
+++ openpdf/src/test/java/com/lowagie/text/html/HtmlParserTest.java
@@ -1,6 +1,7 @@
 package com.lowagie.text.html;
 
 import java.io.StringReader;
+import java.util.EmptyStackException;
 
 import com.lowagie.text.Document;
 import com.lowagie.text.TextElementArray;
@@ -31,6 +32,17 @@ class HtmlParserTest {
         Document doc1 = new Document();
         doc1.open();
         HtmlParser.parse(doc1, new StringReader("<table> <tr><td>test</td></tr></table>")); // was throwin exception
+        assertNotNull(doc1);
+    }
+    
+    /**
+     * Bug fix scenario: a img within two spans throws a {@link EmptyStackException}
+     */
+    @Test
+    void testParse_imgInTwoSpans() {
+        Document doc1 = new Document();
+        doc1.open();
+        HtmlParser.parse(doc1, new StringReader("<span><span><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/200px-PDF_file_icon.svg.png"/></span></span>")); // is throwing exception
         assertNotNull(doc1);
     }
 
